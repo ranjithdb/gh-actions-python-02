e26eca6936d0e2a825bf51f1000cd4e2237c3393
ranjithdb <ranjithdbas@gmail.com>
2025-02-28 04:11:55 -0800

    change python version in github action workflow

--- app.py
+++ app.py
@@ -1,7 +1,7 @@
 import requests
 
 
-def get_joke():    
+def get_joke():
     url = "https://official-joke-api.appspot.com/random_joke"
     response = requests.get(url)
     if response.status_code == 200:
